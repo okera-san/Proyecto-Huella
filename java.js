b3ab4ab2e398b923b86ac6d5309631320a879de2
okera-san <abrahamquezada111@gmail.com>
2025-06-22 17:18:56 -0600

    Actualizacion del proyecto con nuevas mejoras

---
+++ java.js
@@ -0,0 +1,34 @@
+
+    const items = document.querySelectorAll('#tarjetas li');  
+    const btnAnterior = document.getElementById('anterior');
+    const btnSiguiente = document.getElementById('siguiente');
+    let current = 0;
+    let intervalo;
+
+    function mostrar(index) {
+        items[current].classList.remove('active');
+        current = (index + items.length) % items.length;
+        items[current].classList.add('active');
+    }
+
+    function siguiente() {
+        mostrar(current + 1);
+        reiniciarIntervalo();
+    }
+    
+    function anterior(){
+        mostrar(current - 1);
+        reiniciarIntervalo();
+    }
+
+    function reiniciarIntervalo() {
+        clearInterval(intervalo);
+        intervalo = setInterval(() => mostrar(current + 1), 3000);
+    }
+
+    items[current].classList.add('active');
+
+    intervalo = setInterval(() => mostrar(current + 1), 3000);
+
+    btnSiguiente.addEventListener('click', siguiente);
+    btnAnterior.addEventListener('click', anterior);
